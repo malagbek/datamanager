2c8423e1812d8a4d15ec54fae2d5e45f4ea06cd3
malagbek <malagbek@gmail.com>
2018-01-09 22:20:34 +0100

    Create buffer.h
    Abstract class representing some memory with layout information

---
+++ buffer.h
@@ -0,0 +1,34 @@
+#ifndef DATAMANAGER_BUFFER_H
+#define DATAMANAGER_BUFFER_H
+
+#include "types_c.h"
+
+class Buffer
+{
+public:
+    virtual void ~Buffer() {}
+
+    /// number of dimensions
+    virtual int NumDimensions() const = 0;
+    /// length of given dimension
+    virtual int DimLength(int dimension) const = 0;
+    /// total number of stored elements
+    virtual int NumElements() const = 0;
+    /// type of stored data
+    virtual Scalar_t ElementType() const = 0;
+    /// pointer to data start
+    virtual void * Data() const = 0;
+    /// size of individual element in bytes 
+    virtual int ElementBytes() const = 0;
+    
+    /// sets number of dimensions
+    virtual void NumDimensions(int count) = 0;
+    /// sets dimension length
+    virtual void DimLength(int dim, int length) = 0;
+    /// sets element type
+    virtual void ElementType(Scalar_t type) = 0;
+    /// sets data pointer
+    virtual void Data(void * ptr) = 0;
+};
+
+#endif
